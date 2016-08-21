bf57162bccfbac24771717cb288075ac4aa9f815
Silver-Sonic <github@stefan.heinrichsen.net>
2016-08-21 11:37:18 +0200

    * added format checks for input values
    Signed-off-by: Silver-Sonic <github@stefan.heinrichsen.net>

--- StoreLogValues.php
+++ StoreLogValues.php
@@ -12,18 +12,18 @@ $timestamp = time();
 $temp     = ""; // Values should be -200 ... +1000 (divide by 10!)
 $hum      = ""; // Values should be 0 ... 1000 (divide by 10!)
 $sensorID = ""; // ID should be 0...99
-$vcc      = ""; // Raw value of ADC, unit is 1/1024 Volt
+$vcc      = ""; // Raw value of ADC, unit is 1/1024 Volt, int
 
 if(isset($_GET["temp"])) { $temp = $_GET["temp"]; } else { $temp = "--"; }
 if(isset($_GET["hum"] )) { $hum  = $_GET["hum"];  } else { $hum  = "--"; }
 if(isset($_GET["vcc"] )) { $vcc  = $_GET["vcc"];  } else { $vcc  = "--"; }
 
-// Verify range and format of temperature and huminity
-//
-// Regexp for Temp ([0..9]+.[0..9]+)
-// Regexp for Hum  ([0..9]+.[0..9]+)
-// Regexp for Sensor ([0..9]+)
-// Regexp for Vcc ([0..9]{1-5})
+// Verify range and format of temperature, huminity and vcc
+if(preg_match("/^[0-9]{1,3}.[0-9]{1,2}+$/", $temp) != 1) { echo "Invalid Temp Value!"; exit(); }
+if(preg_match("/^[0-9]{1,3}.[0-9]{1,2}+$/", $hum ) != 1) { echo "Invalid Hum Value!" ; exit(); }
+if(preg_match("/^[0-9]{1,5}$/"            , $vcc ) != 1) { echo "Invalid Vcc Value!" ; exit(); }
+
+// Regexp for Sensor ([0..9]+) - Not yet implemented
 
 // a simple log for now
 $logfile = fopen("/home/stefan/www/webroot/temp.heinrichsen.net/temp_log.txt","a");
